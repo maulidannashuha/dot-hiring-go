fad543f270e8970ce6f897e4a6661e8b15042a68
Maulidan Nashuha <maulidannashuha@gmail.com>
2022-12-15 09:22:34 +0700

    book controller temporary testing

--- controllers/book_controller_test.go
+++ controllers/book_controller_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/go-playground/assert/v2"
 )
@@ -16,12 +17,18 @@ var bookController = BookController{}
 func TestBookGetAll(t *testing.T) {
 	r := tests.SetUp()
 
-	models.DB.Create(&models.User{
-		Name:  "John",
-		Email: "johndoe@admin.com",
-	})
+	user := models.User{
+		Name:       "John",
+		Email:      "johndoe@admin.com",
+		DefaultField: models.DefaultField{
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+	}
 
-	mockResponse := `{"message":"Welcome..."}`
+	models.DB.Create(&user)
+
+	mockResponse := `{"data":{"id":2,"created_at":"time","updated_at":"2022-12-15T04:13:25.191563084+07:00","deleted_at":null,"name":"maulidan nashuha","email":"maulidannashuha@gmail.com","books":[]}`
 	r.GET("/", bookController.GetAll)
 	req, _ := http.NewRequest("GET", "/", nil)
 	w := httptest.NewRecorder()
